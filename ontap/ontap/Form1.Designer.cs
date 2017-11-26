df30adb6dbd3380c79e324b6b940c2f786545f04
VINADES.,JSC <contact@vinades.vn>
2017-11-26 11:36:30 +0700

    them bai cau 1 cua de 1

---
+++ ontap/ontap/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace ontap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtchieucao = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnhienthi = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtchieucao
+            // 
+            this.txtchieucao.Location = new System.Drawing.Point(65, 47);
+            this.txtchieucao.Name = "txtchieucao";
+            this.txtchieucao.Size = new System.Drawing.Size(121, 20);
+            this.txtchieucao.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(236, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(11, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "*";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnhienthi
+            // 
+            this.btnhienthi.Location = new System.Drawing.Point(161, 106);
+            this.btnhienthi.Name = "btnhienthi";
+            this.btnhienthi.Size = new System.Drawing.Size(75, 23);
+            this.btnhienthi.TabIndex = 2;
+            this.btnhienthi.Text = "Hiển thị";
+            this.btnhienthi.UseVisualStyleBackColor = true;
+            this.btnhienthi.Click += new System.EventHandler(this.btnhienthi_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 242);
+            this.Controls.Add(this.btnhienthi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtchieucao);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtchieucao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnhienthi;
+    }
+}
+
---
+++ ontap/ontap/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ontap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnhienthi_Click(object sender, EventArgs e)
+        {
+            int h = Int32.Parse(txtchieucao.Text);
+            string s = "";
+            for (int i = 0; i < h; i++)
+            {
+                if (i == 0 || i == h - 1)
+                {
+                    for (int j = 0; j < h; j++)
+                    {
+                        s += "* \t\t\t";                   
+                    }
+                }
+                else if (i % 2 == 0)
+                {
+                    for (int j = 0; j < h; j++)
+                    {
+                        if (i == j)
+                        {
+                            s += "* \t\t\t";
+                        }
+                        else
+                        {
+                            s += "  \t\t\t";
+                        }
+                        
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < h; j++)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            s += "* \t\t\t";
+                        }
+                        else
+                        {
+                            s += "  \t\t\t";
+                        }
+                    }
+                }
+                s += "\n";
+            }
+            label1.Text = s;
+        }
+    }
+}
